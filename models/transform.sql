ce464d06d7553782392c1ca8911a6ce8cd8df7c8
suhaniskorde <suhani.korde@hevodata.com>
2026-03-31 10:16:02 +0530

    Update transform.sql

--- models/transform.sql
+++ models/transform.sql
@@ -1,7 +1,7 @@
-SELECT
-    id,
-    book_name,
-    author,
-    price,
-    price * 0.9 AS discounted_price
-FROM raw_books
+select
+  id,
+  book_name,
+  author,
+  price,
+  price * 0.9 as discounted_price
+from raw_books
